b3a0ebc5f3d802d16857f3f44e9047c9d3869a97
looo89 <looo89@yandex.ru>
2022-04-20 10:00:32 +0300

    Первый коммит

---
+++ src/components/Circle.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from './Circle.module.css'
+
+
+const Circle =(props)=>{
+    return <div className={classes.myCircle}>
+        Circle
+    </div>
+}
+
+
+export default Circle;
---
+++ src/components/buttons/StartButton/StartButton.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import classes from "./StartButton.module.css"
+
+
+const StartButton=(props)=>{
+    return <div className={classes.myButton}>
+        Start
+    </div>
+}
+
+export default StartButton;
